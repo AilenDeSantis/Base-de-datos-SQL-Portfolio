7d06ab3c7e32463014fc8db0933e3a2d820276ae
Ailen De Santis <ailenm.de.santis@gmail.com>
2023-01-23 16:51:55 -0300

    Add files via upload

---
+++ Base de datos portfolio.sql
@@ -0,0 +1,7 @@
+insert into proyecto
+values (2, 'Developets', 'Group final project carried out during the course of the "Soy Henry" bootcamp.
+It is an e-commerce or online petshop called "Developets" for the purchase of various products for pets. Built to be used as both user and administrator.
+It includes searches, filters, favorites, shopping cart, payment gateway, comments and reviews section with their respective rating, admin dashboard with the functionalities relevant.
+Technologies: Javascript | React | Redux | Node.js | HTML | CSS | Express | Sequelize | Postrgres |',
+ 'https://github.com/alberto8812/Pet-store-ecommerce', 1);
+select * from proyecto;
